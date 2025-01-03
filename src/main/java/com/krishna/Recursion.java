2135d3e248f7fdff87972bb17d86ef60e90fee51
Krishna-Madhav <getgoingzazz@gmail.com>
2025-01-03 22:40:11 +0100

    Basic example of recursion (factorial)

--- src/main/java/com/krishna/Recursion.java
+++ src/main/java/com/krishna/Recursion.java
@@ -1,4 +1,18 @@
 package com.krishna;
 
 public class Recursion {
+
+    public static void main(String[] args) {
+
+        int result = factorial(10);
+        System.out.println(result);
+    }
+
+    private static int factorial(int i) {
+
+        if (i > 0)
+            return i * factorial(i - 1);
+
+        return 1;
+    }
 }
